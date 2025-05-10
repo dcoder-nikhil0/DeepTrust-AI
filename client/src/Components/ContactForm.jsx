315cdc7c8c2425209df1ed0dc7d02389050e6822
DcoderLLC <dcoderllc@gmail.com>
2025-05-11 02:31:42 +0530

    contact form completed

--- client/src/Components/ContactForm.jsx
+++ client/src/Components/ContactForm.jsx
@@ -1,208 +1,121 @@
-import React from "react";
-import {
-  Box,
-  Grid,
-  TextField,
-  Button,
-  Typography,
-  Container,
-  MenuItem,
-  Select,
-  FormControl,
-  InputLabel,
-} from "@mui/material";
-import { styled } from "@mui/system";
-import emailjs from "emailjs-com";
-// import ast from "../../assets/assets1.webp";
-
-const StyledSection = styled(Box)({
-  backgroundColor: "#f9fafb",
-  borderRadius: "16px",
-  padding: "24px",
-  margin: "24px",
-  textAlign: "center",
-});
-
-const StyledImage = styled("img")({
-  width: "100%",
-  height: "100%",
-  objectFit: "cover",
-  borderRadius: "16px",
-});
+import React, { useRef } from "react";
+import { Box } from "@mui/material";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
-  const [service, setService] = React.useState("");
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [contact, setContact] = React.useState("");
-  const [message, setMessage] = React.useState("");
-  const [formErrors, setFormErrors] = React.useState({
-    name: false,
-    email: false,
-    contact: false,
-    message: false,
-  });
+  const form = useRef();
 
-  const handleServiceChange = (event) => {
-    setService(event.target.value);
-  };
-
-  const handleSubmit = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    // Basic form validation
-    if (!name || !email || !contact || !message) {
-      setFormErrors({
-        name: !name,
-        email: !email,
-        contact: !contact,
-        message: !message,
-      });
-      return;
-    }
-
-    // EmailJS configuration
-    const serviceID = "service_yv5cbaw";
-    const templateID = "template_lxtb9na";
-    const userID = "BzeILQkfCQQ4Ezl8w";
-
-    const templateParams = {
-      name,
-      email,
-      contact,
-      service,
-      message,
-    };
-
     emailjs
-      .send(serviceID, templateID, templateParams, userID)
-      .then((response) => {
-        console.log("Email sent successfully:", response.status, response.text);
-        // Reset form fields and errors after successful submission
-        setName("");
-        setEmail("");
-        setContact("");
-        setService("");
-        setMessage("");
-        setFormErrors({
-          name: false,
-          email: false,
-          contact: false,
-          message: false,
-        });
-      })
-      .catch((error) => {
-        console.error("Failed to send email:", error);
-      });
+      .sendForm(
+        "service_15wcdxr",
+        "template_qteedc7",
+        form.current,
+        "kwzg2HZtC8KSNn66L"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Message sent successfully!");
+        },
+        (error) => {
+          console.error(error.text);
+          alert("An error occurred. Please try again.");
+        }
+      );
+
+    e.target.reset();
   };
 
   return (
-    <Box py={8} bgcolor="#ECF0F1">
-      <Container>
-        <Grid container spacing={4} justifyContent="center">
-          <Grid item xs={12} md={6}>
-            <Box
-              height={{ xs: "300px", md: "100%" }}
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-            >
-              <StyledImage src={''} alt="Contact Us" />
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <StyledSection>
-              <Typography variant="h4" component="h1" gutterBottom>
-                Contact Us
-              </Typography>
-              <Typography variant="body1" paragraph>
-                Feel free to reach out to us! Whether you have a question,
-                feedback, or a collaboration proposal, we did love to hear from
-                you.
-              </Typography>
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    label="Name"
-                    variant="outlined"
-                    fullWidth
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    error={formErrors.name}
-                    helperText={formErrors.name && "Name is required"}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    label="Email"
-                    variant="outlined"
-                    fullWidth
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    error={formErrors.email}
-                    helperText={formErrors.email && "Email is required"}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    label="Contact"
-                    variant="outlined"
-                    fullWidth
-                    value={contact}
-                    onChange={(e) => setContact(e.target.value)}
-                    error={formErrors.contact}
-                    helperText={formErrors.contact && "Contact is required"}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <FormControl variant="outlined" fullWidth>
-                    <InputLabel id="service-label">Select Service</InputLabel>
-                    <Select
-                      labelId="service-label"
-                      value={service}
-                      onChange={handleServiceChange}
-                      label="Select Service"
-                    >
-                      <MenuItem value="web-designing">Web Designing</MenuItem>
-                      <MenuItem value="graphic-designing">
-                        Graphic Designing
-                      </MenuItem>
-                      <MenuItem value="video-editing">Video Editing</MenuItem>
-                      <MenuItem value="social-media-handling">
-                        Social Media Handling
-                      </MenuItem>
-                    </Select>
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label="Message"
-                    variant="outlined"
-                    fullWidth
-                    multiline
-                    rows={4}
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    error={formErrors.message}
-                    helperText={formErrors.message && "Message is required"}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    size="large"
-                    fullWidth
-                    onClick={handleSubmit}
+    <Box>
+      <section className="bg-white">
+        <div className="container px-6 py-12 mx-auto">
+          <div className="lg:flex lg:items-center lg:-mx-6">
+            {/* Left side: Contact Info Only */}
+            <div className="lg:w-1/2 lg:mx-12">
+              <h1 className="text-3xl font-semibold text-gray-800 ">
+                Get in Touch
+              </h1>
+              <div className="space-y-8 md:mt-8">
+                <p className="flex items-start -mx-2 text-gray-700 ">
+                  <span className="mx-2 text-base">📞</span>
+                  +91 75448 38483
+                </p>
+
+                <p className="flex items-start -mx-2 text-gray-700 ">
+                  <span className="mx-2 text-base">✉️</span>
+                  support@kshitizz.com
+                </p>
+
+                <p className="flex items-start -mx-2 text-gray-700 ">
+                  <span className="mx-2 text-base">📍</span>
+                  Patna, India
+                </p>
+              </div>
+            </div>
+
+            {/* Right side: Email Form */}
+            <div className="mt-8 lg:w-1/2 lg:mx-6">
+              <div className="w-full px-8 py-10 mx-auto overflow-hidden bg-white rounded-lg shadow-2xl lg:max-w-xl shadow-black/50">
+                <h1 className="text-lg font-medium text-gray-800">
+                  Feel free to reach out!
+                </h1>
+
+                <form ref={form} onSubmit={sendEmail} className="mt-6">
+                  <div className="mb-4">
+                    <label className="block mb-2 text-sm text-gray-600 ">
+                      Full Name
+                    </label>
+                    <input
+                      type="text"
+                      name="user_name"
+                      required
+                      placeholder="John Doe"
+                      className="block w-full px-5 py-3 text-gray-700  bg-white border border-gray-200 rounded-md placeholder-gray-600 "
+                    />
+                  </div>
+
+                  <div className="mb-4">
+                    <label className="block mb-2 text-sm text-gray-600 ">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      name="user_email"
+                      required
+                      placeholder="john@example.com"
+                      className="block w-full px-5 py-3 text-gray-700  bg-white border border-gray-200 rounded-md placeholder-gray-600"
+                    />
+                  </div>
+
+                  <div className="mb-4">
+                    <label className="block mb-2 text-sm text-gray-600 ">
+                      Message
+                    </label>
+                    <textarea
+                      name="message"
+                      rows="4"
+                      required
+                      placeholder="Your message here..."
+                      className="block w-full px-5 py-3 text-gray-700  bg-white border border-gray-200 rounded-md placeholder-gray-600 "
+                    ></textarea>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
                   >
-                    Submit
-                  </Button>
-                </Grid>
-              </Grid>
-            </StyledSection>
-          </Grid>
-        </Grid>
-      </Container>
+                    Send Message
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </Box>
   );
 };
